7cc358cb04206cef1651f3f70b12831943bf270f
jaganrajaa15-arch <jaganrajaa15@gmail.com>
2026-07-22 10:12:52 +0530

    pull

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,20 @@
 import { useState } from 'react'
 
 import './App.css'
+import AboutUs from './Pages/AboutUs/AboutUs'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-  <>
-  
-  </>
+ <BrowserRouter>
+      <Routes>
+        
+      <Route path="/" element={<AboutUs/>} />
+
+
+      </Routes>
+    </BrowserRouter>
   )
 }
 
