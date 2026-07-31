2a3a96ed15a44fd536d0b33e5f8b10310f3cf100
roshan-dev12 <pn2279747@gmail.com>
2026-07-31 15:25:36 +0530

    done

--- Frontend/src/Component/AboutFindDreamHome/AboutFindDreamHome.jsx
+++ Frontend/src/Component/AboutFindDreamHome/AboutFindDreamHome.jsx
@@ -13,7 +13,7 @@ export function AboutFindDreamHome() {
         {/* Left Side: Text & Call to Action */}
         <div className="AboutFindDreamHome-content">
           <h1 className="AboutFindDreamHome-title">
-            Find for your dream home and increase your investment opportunities
+            Find for your <span className="AboutFindDreamHome-highlight-text">dream home</span> and increase your investment opportunities
           </h1>
           <p className="AboutFindDreamHome-subtitle">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sed tristique metus proin id lorem odio
@@ -31,7 +31,7 @@ export function AboutFindDreamHome() {
             <svg width="100%" height="100%" viewBox="0 0 400 600" xmlns="http://www.w3.org/2000/svg">
               <defs>
                 <pattern id="isometricGrid" width="60" height="104" patternUnits="userSpaceOnUse">
-                  <path d="M30 0l30 17.32v34.64L30 69.28 0 51.96V17.32L30 0zm0 34.64l30-17.32M30 34.64v34.64M30 34.64L0 17.32" stroke="#fca311" strokeWidth="1.5" fill="none" fillRule="evenodd" />
+                  <path d="M30 0l30 17.32v34.64L30 69.28 0 51.96V17.32L30 0zm0 34.64l30-17.32M30 34.64v34.64M30 34.64L0 17.32" stroke="#10532e" strokeWidth="1.5" fill="none" fillRule="evenodd" />
                 </pattern>
               </defs>
               <rect width="100%" height="100%" fill="url(#isometricGrid)" />
