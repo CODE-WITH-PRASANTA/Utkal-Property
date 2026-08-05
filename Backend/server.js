8c752ab9d6d80a7b10c7f7963306755e467a69b7
Prasanta Kumar Khuntia <prasantakhuntia2018@gmail.com>
2026-08-05 11:03:04 +0530

    Done

--- Backend/server.js
+++ Backend/server.js
@@ -49,7 +49,7 @@ app.get("/", (req, res) => {
   res.json({ message: "Server Running Successfully" });
 });
 
-const PORT = process.env.PORT || 6014;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
